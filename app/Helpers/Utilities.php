27d5594c2564ae25ec2ce98d55450ce25475bc4a
Aldi Unanto <aldiunanto@yahoo.com>
2015-08-06 14:06:35 +0700

    Add Utilities helper to the stagging area

---
+++ app/Helpers/Utilities.php
@@ -0,0 +1,95 @@
+<?php
+
+	function now($time = false){
+        return date('Y-m-d' . ($time == true ? ' H:i:s' : ''));
+    }
+    function images_url($uri = ''){
+    	return 'http://' . $_SERVER['HTTP_HOST'] . '/jaly_images/' . $uri;
+    }
+    function notif($type, $msg){
+    	return '<div class="notif ' . $type . '"><p>' . $msg . '</p></div>';
+    }
+    function current_route($type){
+    	$path  = explode('@', Route::currentRouteAction());
+    	$pathc = explode("\\", $path[0]);
+
+    	if($type == 'class') return strtolower(end($pathc));
+    	else return end($path);
+    }
+    function is_active($expect){
+        if(current_route('class') == $expect) return 'class="active"';
+    }
+    function convert_date($the_date, $date2, $view = 'short'){ 
+        if($date2 == '') return '-';
+        
+        $str = '';
+        switch($view){
+            case 'short' :
+                $d['y'] = '-T';
+                $d['m'] = '-B';
+                $d['d'] = '-H';
+            break;
+            default :
+                $d['y'] = ' Tahun';
+                $d['m'] = ' Bulan';
+                $d['d'] = ' Hari';
+            break;
+        }
+            
+        $now = new DateTime($date2);
+        $ref = new DateTime($the_date);
+        $diff = $now->diff($ref);
+            
+        $date1 = strtotime($date2);
+        $date2 = strtotime($the_date);
+            
+        if($date2 <= $date1){
+            if($diff->y > 0){ $str .= $diff->y.$d['y'].', '; }
+            if($diff->m > 0){ $str .= $diff->m.$d['m'].', '; }
+            if($diff->d == 0){
+                $str .= ($diff->d + 1).$d['d'];
+            }else{
+                $str .= $diff->d.$d['d'];
+            }
+        }else{
+            $str .= '<span style="font-size: 10px; font-style: italic;">(Belum masuk kerja)</span>';
+        }
+            
+        return $str;
+    }
+    function to_indDate($date){
+        $path = explode('-', $date);
+        return $path[2] . '-' . $path[1] . '-' . $path[0];
+    }
+    function romawi(){
+        return [1 => 'I', 2 => 'II', 3 => 'III', 4 => 'IV', 5 => 'V', 6 => 'VI', 7 => 'VII', 8 => 'VIII', 9 => 'IX', 10 => 'X', 11 => 'XI', 12 => 'XII'];
+    }
+    function dateDiff($permintaan, $kedatangan){
+        $str = '';
+        $d = ['y' => ' Tahun', 'm' => ' Bulan', 'd' => ' Hari'];
+            
+        $now = new DateTime($kedatangan);
+        $ref = new DateTime($permintaan);
+        $diff = $now->diff($ref);
+        
+        $date1 = strtotime($kedatangan);
+        $date2 = strtotime($permintaan);
+        
+        if($date1 > $date2){
+            $str .= '<span class="status pm-reject-vice label">Terlambat ';
+        }elseif($date1 < $date2){
+            $str .= '<span class="status po-done label">Lebih cepat ';
+        }else{
+            return '<span class="status vice-approve label">Tepat waktu';
+        }
+
+        if($diff->y > 0){ $str .= $diff->y.$d['y'].', '; }
+        if($diff->m > 0){ $str .= $diff->m.$d['m'].', '; }
+        if($diff->d == 0){
+            $str .= ($diff->d + 1).$d['d'];
+        }else{
+            $str .= $diff->d.$d['d'];
+        }
+
+        return $str . '</span>';
+    }
