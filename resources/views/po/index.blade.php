7fe4f41048c7a57d31740c3db5df94eee6e0a63f
Aldi Unanto <aldiunanto@yahoo.com>
2015-08-10 11:31:30 +0700

    Fixed all of data list to be displayed

--- resources/views/po/index.blade.php
+++ resources/views/po/index.blade.php
@@ -51,6 +51,8 @@
 				$y = '';
 			}
 
+			$accepted = countDiterima($row->pos_id);
+
 		?>
 
 			@if(! empty($y))
@@ -74,14 +76,48 @@
 			</tr>
 			@endif
 
-			<tr data-po="{{ $row->po_id }}" class="hidden child">
+			<tr data-po="{{ $row->po_id }}" class="child">
 				<td>&nbsp;</td>
 				<td class="text-right">{{ $x . '.' . ++$z . '.' }}</td>
 				<td>{{ $row->mat_nama }}</td>
 				<td class="text-center">{{ to_indDate($row->po_tgl_buat) }}</td>
 				<td>{{ $row->pbs_jml }}</td>
-				<td><a class="mat-acceptance" href="{{ url('po/matAcceptanceDetail/' . $row->pos_id) }}" title="Klik untuk lihat detail penerimaan material ini"><i class="fa fa-eye"></i> {{ countDiterima($row->pos_id) }}</a></td>
-				<td class="text-center"><span class="status pm-reject-vice" title="Kurang 200, telat 2 hari">-200, -2D</span></td>
+				<td><a class="mat-acceptance" href="{{ url('po/matAcceptanceDetail/' . $row->pos_id) }}" title="Klik untuk lihat detail penerimaan material ini"><i class="fa fa-eye"></i> {{ $accepted }}</a></td>
+				<td class="text-center">
+					<?php
+
+						$diff = $row->pbs_jml - $accepted;
+						if($diff == 0){
+							$text['f']['t'] = 'Terpenuhi, ';
+							$text['f']['c'] = 'Closed, ';
+							$text['f_type'] = 'closed';
+						}elseif($diff > 0){
+							$text['f']['t'] = 'Kurang ' . $diff . ', ';
+							$text['f']['c'] = '-' . $diff . ', ';
+							$text['f_type'] = 'min';
+						}
+
+						$first = ($row->po_status == 1 ? now() : $row->pener_date);
+						$text += poDateDiff($first, $row->po_tgl_kedatangan);
+
+						if($text['f_type'] == 'min' && $text['s_type'] == 'slow'){
+							$class = 'pm-reject-vice';
+						}elseif($text['f_type'] == 'min' && $text['s_type'] == 'fast'){
+							$class = 'pm-reject-ppic';
+						}elseif($text['f_type'] == 'min' && $text['s_type'] == 'ontime'){
+							$class = 'wait-vice';
+						}elseif($text['f_type'] == 'closed' && $text['s_type'] == 'ontime'){
+							$class = 'po-done';
+						}elseif($text['f_type'] == 'closed' && $text['s_type'] == 'fast'){
+							$class = 'vice-approve';
+						}elseif($text['f_type'] == 'closed' && $text['s_type'] == 'slow'){
+							$class = 'wait-ppic';
+						}
+
+						echo '<span class="status '.$class.'" title="'.$text['f']['t'].$text['s']['t'].'">'.$text['f']['c'].$text['s']['c'].'</span>';
+
+					?>
+				</td>
 				<td class="text-right">&nbsp;</td>
 			</tr>
 
@@ -95,7 +131,11 @@
 <!--
 <span class="status pm-reject-vice" title="Kurang 200, telat 2 hari">-200, -2D</span>
 <span class="status pm-reject-ppic" title="Kurang 200, lebih awal 2 hari">-200, +2D</span>
-<span class="status vice-approve" title="Terpenuhi, tepat waktu">Closed, tepat</span>
+<span class="status wait-vice" title="Kurang 200, tepat watu">-200, tepat</span>
+
+<span class="status po-done" title="Terpenuhi, tepat waktu">Closed, tepat</span>
+<span class="status vice-approve" title="Terpenuhi, lebih awal 3 hari">Closed, +3D</span>
+<span class="status wait-ppic" title="Terpenuhi, telat 3 hari">Closed, -3D</span>
 -->
 
 @endsection
