b80770e5d46b3fd644ecc5e20f796d66e895c007
Aldi Unanto <aldiunanto@yahoo.com>
2015-08-11 09:02:41 +0700

    Adjusting for dataTables list

--- resources/views/po/index.blade.php
+++ resources/views/po/index.blade.php
@@ -2,21 +2,6 @@
 
 @section('content')
 <div class="top">
-	<div class="tools">
-		<div class="search" <?php echo (! is_null($search['s']) ? 'style="display: block;"' : '') ?>>
-			<form action="{{ url('po') }}" method="get">
-				<i class="fa fa-close"></i>
-				<select name="field">
-					<option value="po_no" <?php echo $isSelected('po_no') ?>>Nomor PO</option>
-					<option value="po_tgl_butuh" <?php echo $isSelected('po_tgl_butuh') ?>>Tanggal Order</option>
-					<option value="mat_nama" <?php echo $isSelected('mat_nama') ?>>Nama Material</option>
-				</select>
-				<input type="text" name="s" value="{{ $search['s'] }}" placeholder="Kata pencarian.." />
-				<button><i class="fa fa-search"></i></button>
-			</form>
-		</div>
-		<a href="javascript:;" class="btn btn-search warning" <?php echo (! is_null($search['s']) ? 'style="display: none;"' : '') ?>><i class="fa fa-search"></i> Pencarian</a>
-	</div>
 	<h2>Data List PO <span>{{ $fetch->count() }} dari {{ $fetch->total() }}</span></h2>
 	<div class="clearfix"></div>
 </div>
@@ -36,49 +21,22 @@
 			</tr>
 		</thead>
 		<tbody>
-		<?php $z = 0; $x = $getNumb(); $po_no = ''; ?>
+
+		<?php $x = 0; $po_no = ''; ?>
 		@foreach($fetch as $row)
-		<?php
-
-			$curr_po_no = $row->po_no;
-			if($po_no != $curr_po_no){
+			<?php $accepted = countDiterima($row->pos_id) ?>
+			@if($po_no != $row->po_no)
+			<?php
+				$tr_class = 'new-po';
 				$po_no = $row->po_no;
-				$x++;
-				$y = $x . '.';
-				$z = 0;
-			}else{
-				$curr_po_no = '';
-				$y = '';
-			}
-
-			$accepted = countDiterima($row->pos_id);
-
-		?>
-
-			@if(! empty($y))
-			<tr data-po="{{ $row->po_id }}">
-				<td class="text-right">{{ $y }}</td>
-				<td><a href="javascript:;" class="expand" data-po="{{ $row->po_id }}" title="Lihat Material"><i class="fa fa-navicon"></i>&nbsp;&nbsp;{{ $curr_po_no }}</a></td>
-				<td colspan="5">&nbsp;</td>
-				<td class="text-right">
-					<ul class="actions">
-						<li><span><i class="fa fa-angle-down"></i></span>
-							<ul>
-								<li><a href="{{ url('po/show/' . $row->po_id) }}" class="view-detail"><i class="fa fa-eye"></i>Detail PO</a></li>
-								<li><a href="{{ url('') }}"><i class="fa fa-list"></i>Detail Penerimaan</a></li>
-								
-								<li class="separator">&nbsp;</li>
-								<li><a href="{{ url('printing/po/' . $row->po_id) }}" target="_blank" ><i class="fa fa-print"></i>Cetak PO</a></li>
-							</ul>
-						</li>
-					</ul>
-				</td>
-			</tr>
+			?>
+			@else
+			<?php $tr_class = ''; ?>
 			@endif
 
-			<tr data-po="{{ $row->po_id }}" class="child">
-				<td>&nbsp;</td>
-				<td class="text-right">{{ $x . '.' . ++$z . '.' }}</td>
+			<tr class="{{ $tr_class }}">
+				<td class="text-center">{{ ++$x }}.</td>
+				<td>{{ $po_no }}</td>
 				<td>{{ $row->mat_nama }}</td>
 				<td class="text-center">{{ to_indDate($row->po_tgl_buat) }}</td>
 				<td>{{ $row->pbs_jml }}</td>
@@ -118,7 +76,19 @@
 
 					?>
 				</td>
-				<td class="text-right">&nbsp;</td>
+				<td class="text-right">
+					<ul class="actions">
+						<li><span><i class="fa fa-angle-down"></i></span>
+							<ul>
+								<li><a href="{{ url('po/show/' . $row->po_id) }}" class="view-detail"><i class="fa fa-eye"></i>Detail PO</a></li>
+								<li><a href="{{ url('') }}"><i class="fa fa-list"></i>Detail Penerimaan</a></li>
+								
+								<li class="separator">&nbsp;</li>
+								<li><a href="{{ url('printing/po/' . $row->po_id) }}" target="_blank" ><i class="fa fa-print"></i>Cetak PO</a></li>
+							</ul>
+						</li>
+					</ul>
+				</td>
 			</tr>
 
 		@endforeach
