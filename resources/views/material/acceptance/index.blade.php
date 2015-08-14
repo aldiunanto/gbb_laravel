b1fa1159fc74a79ba0496fb32945c48438f4baea
Aldi Unanto <aldiunanto@yahoo.com>
2015-08-14 10:17:34 +0700

    Remove search form caused using dataTables

--- resources/views/material/acceptance/index.blade.php
+++ resources/views/material/acceptance/index.blade.php
@@ -3,19 +3,6 @@
 @section('content')
 <div class="top">
 	<div class="tools">
-		<div class="search" <?php echo (! is_null($search['s']) ? 'style="display: block;"' : '') ?>>
-			<form action="{{ url('material/acceptance') }}" method="get">
-				<i class="fa fa-close"></i>
-				<select name="field">
-					<option value="po_no" <?php echo $isSelected('po_no') ?>>Nomor PO</option>
-					<option value="po_tgl_kedatangan" <?php echo $isSelected('po_tgl_kedatangan') ?>>Tanggal Permintaan</option>
-					<option value="pener_date" <?php echo $isSelected('pener_date') ?>>Tanggal Penerimaan</option>
-				</select>
-				<input type="text" name="s" value="{{ $search['s'] }}" placeholder="Kata pencarian.." />
-				<button><i class="fa fa-search"></i></button>
-			</form>
-		</div>
-		<a href="javascript:;" class="btn btn-search warning" <?php echo (! is_null($search['s']) ? 'style="display: none;"' : '') ?>><i class="fa fa-search"></i> Pencarian</a>
 		<a href="{{ url('material/acceptance/create') }}" class="btn default"><i class="fa fa-plus"></i> tambah penerimaan</a>
 	</div>
 	<h2>Daftar Penerimaan Material <span>{{ $fetch->count() }} dari {{ $fetch->total() }}</span></h2>
