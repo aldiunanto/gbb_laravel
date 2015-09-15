9f954d95125fd16c217a08ab87757ad517f04b85
Aldi Unanto <aldiunanto@yahoo.com>
2015-09-15 11:56:30 +0700

    Sketch a delivery order

---
+++ resources/views/printing/do.blade.php
@@ -0,0 +1,135 @@
+@extends('printing')
+@section('content')
+
+<header>
+	<div class="caption">
+		<h1>surat jalan</h1>
+		<h2>no: 229/VIII/JIU/2015</h2>
+	</div>
+	<table class="base-info">
+		<tr>
+			<td class="profile-info left">
+				Industri Sepatu dan Keselamatan Kerja<br />
+				PT. JALY INDONESIA UTAMA<br />
+				JL. HM. Ashari No.47 Cibinong-Bogor<br />
+				Telp: 021-2154501 Fax: 021-8752174
+			</td>
+			<td class="supp-info">
+				<table>
+					<tr>
+						<td>Kepada Yth</td>
+						<td>:</td>
+						<td>PT. PILAR MAKMUR</td>
+					</tr>
+					<tr>
+						<td>Alamat</td>
+						<td>:</td>
+						<td>Tangerang</td>
+					</tr>
+					<tr>
+						<td>Phone</td>
+						<td>:</td>
+						<td>021-8765789</td>
+					</tr>
+					<tr>
+						<td>Attn</td>
+						<td>:</td>
+						<td>Bp. Bandi/Bp. Musa</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+</header>
+<section class="content">
+	<div class="info">
+		<span class="date">Tanggal&emsp;&emsp;&nbsp;: 21-05-2015</span>
+		<span class="po-no">Nomor PO&emsp;: P/404/VIII/2015</span>
+	</div>
+	<table>
+		<thead>
+			<tr>
+				<th>Item</th>
+				<th>Spec</th>
+				<th>QTY</th>
+				<th>Unit</th>
+				<th>Keterangan</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>O/S RUBBER EXECUTIVE LADIES</td>
+				<td>Size 39</td>
+				<td>3</td>
+				<td>Pcs</td>
+				<td>Pleteran Belakang/Reject</td>
+			</tr>
+			<tr>
+				<td>O/S RUBBER EXECUTIVE LADIES</td>
+				<td>Size 41</td>
+				<td>32</td>
+				<td>Pcs</td>
+				<td>Pleteran Belakang/Reject</td>
+			</tr>
+			<tr>
+				<td>O/S RUBBER EXECUTIVE LADIES</td>
+				<td>Size 42</td>
+				<td>9</td>
+				<td>Pcs</td>
+				<td>Pleteran Belakang/Reject</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+		</tbody>
+	</table>
+</section>
+<footer>
+	<table>
+		<tr>
+			<td class="top" rowspan="2">Tanda Terima</td>
+			<td rowspan="2">
+				<h5>TELAH DIPERIKSA</h5>
+				<table>
+					<tr>
+						<td>Gudang</td>
+						<td>Satpam</td>
+						<td>Supir</td>
+					</tr>
+					<tr>
+						<td>&nbsp;</td>
+						<td>&nbsp;</td>
+						<td>&nbsp;</td>
+					</tr>
+				</table>
+			</td>
+			<td class="top">
+				Hormat kami,<br />
+				Staff Purch
+			</td>
+			<td class="top">
+				Mengetahui,
+			</td>
+		</tr>
+		<tr class="name">
+			<td>MELANI</td>
+			<td>LIVIA YUWONO</td>
+		</tr>
+		<tr>
+			<td colspan="4" class="iso-no">Form-ADM-036-0 Rev: 0-Hal: 1 / Agust'15</td>
+		</tr>
+	</table>
+</footer>
+
+@endsection
