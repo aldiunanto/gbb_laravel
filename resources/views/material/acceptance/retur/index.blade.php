5f989ef5fcd3f87b2c651ddaaa7b04e3d6f2f7a9
Aldi Unanto <aldiunanto@yahoo.com>
2015-09-15 14:12:33 +0700

    If had approved by VD and Purchasing login, then show print DO link

--- resources/views/material/acceptance/retur/index.blade.php
+++ resources/views/material/acceptance/retur/index.blade.php
@@ -40,8 +40,12 @@
 								<li><a href="{{ url('material/acceptance/retur/show/' . $row->returpener_id) }}" class="view-retur-detail"><i class="fa fa-eye"></i>Lihat Detail</a></li>
 								<li class="separator">&nbsp;</li>
 
+								@if($role == 2 && $row->returpener_status == 5)
+								<li><a href="{{ url('printing/do/' . $row->returpener_id) }}" target="_blank" ><i class="fa fa-print"></i>Cetak Surat Jalan</a></li>
+								@else
 								<li><a href="{{ url('material/acceptance/retur/accept/' . $row->returpener_id) }}" class="do-approve{{ ($role == 5 ? ' vd' : '') }}"><i class="fa fa-check"></i>Setujui</a></li>
 								<li><a href="{{ url('material/acceptance/retur/reject/' . $row->returpener_id) }}" class="do-reject"><i class="fa fa-remove"></i>Tolak</a></li>
+								@endif
 							</ul>
 						</li>
 					</ul>
