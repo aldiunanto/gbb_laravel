b29ce1bf7ecdac948f3253c832d88445e0474bf9
Aldi Unanto <aldiunanto@yahoo.com>
2015-09-18 13:45:20 +0700

    Create 2nd parameter for pener_id

--- app/Models/Retur_penerimaan_sub.php
+++ app/Models/Retur_penerimaan_sub.php
@@ -9,17 +9,21 @@ class Retur_penerimaan_sub extends Model {
 	protected	$fillable	= ['returpener_id', 'peners_id', 'returpeners_jml', 'returpeners_reason', 'returpeners_is_reduced'];
 	public 		$timestamps	= false;
 
-	public static function fetch($returpener_id){
-		$i = new static;
-		return self::select('E.mat_nama', 'E.mat_spesifikasi', 'F.wrn_nama', 'G.mats_nama', 'B.peners_jml', $i->table.'.returpeners_jml', $i->table.'.returpeners_reason')
+	public static function fetch($returpener_id, $pener_id = null){
+		$i 		= new static;
+		$get 	= self::select('E.mat_nama', 'E.mat_spesifikasi', 'F.wrn_nama', 'G.mats_nama', 'B.peners_jml', $i->table.'.returpeners_jml', $i->table.'.returpeners_reason', 'H.returpener_status')
 					->join('penerimaan_sub_laravel AS B', $i->table.'.peners_id', '=', 'B.peners_id')
 					->join('po_sub_laravel AS C', 'B.pos_id', '=', 'C.pos_id')
 					->join('permintaan_barang_sub AS D', 'C.pbs_id', '=', 'D.pbs_id')
 					->join('material_laravel AS E', 'D.mat_id', '=', 'E.mat_id')
 					->leftJoin('warna AS F', 'E.wrn_id', '=', 'F.wrn_id')
 					->join('material_satuan AS G', 'E.matsp_id', '=', 'G.mats_id')
-					->where($i->table.'.returpener_id', $returpener_id)
-					->get();
+					->join('retur_penerimaan_laravel AS H', $i->table.'.returpener_id', '=', 'H.returpener_id');
+
+		if(! is_null($returpener_id)) $get->where($i->table.'.returpener_id', $returpener_id);
+		if(! is_null($pener_id)) $get->where('H.pener_id', $pener_id);
+
+		return $get->get();
 	}
 	public static function getMatData($returpener_id){
 		$i = new static;
