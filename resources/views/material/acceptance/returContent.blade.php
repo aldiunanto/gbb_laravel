f7305fd298fdcf6074aaf17973ccfe0e6df2d058
Aldi Unanto <aldiunanto@yahoo.com>
2015-09-18 11:08:16 +0700

    Renamed file to .blade extension

---
+++ resources/views/material/acceptance/returContent.blade.php
@@ -0,0 +1,36 @@
+<tr data-retur-content="{{ $pener_id }}">
+	<td colspan="4">
+		<table class="retur-content">
+			<thead>
+				<tr>
+					<th>Material</th>
+					<th>Jml Retur</th>
+					<th>Alasan</th>
+					<th>Status</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Kaos Dalem</td>
+					<td class="text-right">1</td>
+					<td class="text-center">Jelek</td>
+					<td class="text-center">
+						<span class="status wait-ppic" title="Menunggu persetujuan QA">
+							<i class="fa fa-spinner fa-spin"></i>
+						</span>
+					</td>
+				</tr>
+				<tr>
+					<td>Kaos Kutang</td>
+					<td class="text-right">1</td>
+					<td class="text-center">Kotor</td>
+					<td class="text-center">
+						<span class="status wait-ppic" title="Menunggu persetujuan QA">
+							<i class="fa fa-spinner fa-spin"></i>
+						</span>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</td>
+</tr>
