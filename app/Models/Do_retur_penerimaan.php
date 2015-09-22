e5e6ffb643a0a786263001eb67d7dbcf02362b91
Aldi Unanto <aldiunanto@yahoo.com>
2015-09-22 15:15:39 +0700

    Add some protected variables

--- app/Models/Do_retur_penerimaan.php
+++ app/Models/Do_retur_penerimaan.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Do_retur_penerimaan extends Model {
 
-	
-	
+	protected $table		= 'do_retur_penerimaan';
+	protected $primaryKey	= 'dorp_id';
+	protected $fillable		= ['returpener_id', 'dorp_no'];
+
 }
