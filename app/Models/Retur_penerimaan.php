d529470797614803c76d12cffed0dd73e79cd050
Aldi Unanto <aldiunanto@yahoo.com>
2015-09-25 14:34:28 +0700

    Change table's initials

--- app/Models/Retur_penerimaan.php
+++ app/Models/Retur_penerimaan.php
@@ -75,7 +75,7 @@ class Retur_penerimaan extends Model {
 		$i = new static;
 		$get = self::select($i->table.'.'.$i->primaryKey, $i->table.'.created_at', 'B.pener_date', 'C.po_no', 'D.dorp_no', 'F.sup_nama')
 				->join('penerimaan_laravel AS B', $i->table.'.pener_id', '=', 'B.pener_id')
-				->join('po_laravel AS C', 'B.po_id', '=', 'B.po_id')
+				->join('po_laravel AS C', 'B.po_id', '=', 'C.po_id')
 				->join('do_retur_penerimaan AS D', $i->table.'.'.$i->primaryKey, '=', 'D.returpener_id')
 				->join('permintaan_barang AS E', 'C.pb_id', '=', 'E.pb_id')
 				->join('supplier_laravel AS F', 'E.sup_id', '=', 'F.sup_id')
@@ -92,6 +92,7 @@ class Retur_penerimaan extends Model {
 					->orderBy($i->table.'.created_at', 'DESC')
 					->take(20)
 					->get();
+
 	}
 
 }
