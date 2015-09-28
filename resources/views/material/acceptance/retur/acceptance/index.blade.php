db5e5ab4f6e8b0dc4c0487b22703e9a7ff907851
Aldi Unanto <aldiunanto@yahoo.com>
2015-09-28 10:08:44 +0700

    Frame of returAcceptance List

---
+++ resources/views/material/acceptance/retur/acceptance/index.blade.php
@@ -0,0 +1,18 @@
+<table class="data-list index">
+	<thead>
+		<tr>
+			<th>No</th>
+			<th>Nomor PO</th>
+			<th>No. Surat Jalan</th>
+			<th>Supplier</th>
+			<th>Tgl Permintaan</th>
+			<th>Tgl Retur</th>
+			<th>Tgl Kedatangan</th>
+			<th>Keterangan</th>
+			<th>Aksi</th>
+		</tr>
+	</thead>
+	<tbody>
+
+	</tbody>
+</table>
