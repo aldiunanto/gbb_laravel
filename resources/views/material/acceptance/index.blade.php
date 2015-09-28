b614cb920d026766f2f27c3956f6b48bf055fa24
Aldi Unanto <aldiunanto@yahoo.com>
2015-09-28 09:02:40 +0700

    Moved input returan button

--- resources/views/material/acceptance/index.blade.php
+++ resources/views/material/acceptance/index.blade.php
@@ -4,11 +4,12 @@
 <div class="top">
 	@if($role == 3 || $role == 1)
 	<div class="tools">
-		<a href="{{ url('material/acceptance/create') }}" class="btn default"><i class="fa fa-plus"></i> tambah penerimaan</a>
+		<a href="{{ url('material/acceptance/create') }}" class="btn primary"><i class="fa fa-plus"></i> tambah penerimaan</a>
+		<a href="{{ url('material/acceptance/retur/input') }}" class="btn default"><i class="fa fa-plus"></i> tambah penerimaan returan</a>
 	</div>
 	@endif
 	<h2>Daftar Penerimaan Material</h2>
-	<a href="{{ url('material/acceptance/retur') }}">Retur penerimaan &raquo;</a>
+	<a href="{{ url('material/acceptance/retur') }}">Daftar Retur penerimaan &raquo;</a>
 
 	<div class="clearfix"></div>
 </div>
