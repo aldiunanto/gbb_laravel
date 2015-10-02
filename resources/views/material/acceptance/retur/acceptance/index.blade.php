bbe07daa46c2bd01d7e711c16dea3d6ca6c87b74
Aldi Unanto <aldiunanto@yahoo.com>
2015-10-02 14:28:57 +0700

    Fetch acceptance retur data, remove, and change some columns

--- resources/views/material/acceptance/retur/acceptance/index.blade.php
+++ resources/views/material/acceptance/retur/acceptance/index.blade.php
@@ -5,14 +5,33 @@
 			<th>Nomor PO</th>
 			<th>No. Surat Jalan</th>
 			<th>Supplier</th>
-			<th>Tgl Permintaan</th>
+			<th>Tgl Penerimaan</th>
 			<th>Tgl Retur</th>
 			<th>Tgl Kedatangan</th>
-			<th>Keterangan</th>
 			<th>Aksi</th>
 		</tr>
 	</thead>
 	<tbody>
-
+		<?php $x = 0 ?>
+		@foreach($fetch as $row)
+		<tr>
+			<td>{{ ++$x }}.</td>
+			<td>{{ $row->po_no }}</td>
+			<td>{{ $row->dorp_no }}</td>
+			<td>{{ $row->sup_nama }}</td>
+			<td>{{ $row->pener_date }}</td>
+			<td>{{ $row->tglRetur }}</td>
+			<td>{{ $row->peneretur_date }}</td>
+			<td>
+				<ul class="actions">
+					<li><span><i class="fa fa-angle-down"></i></span>
+						<ul>
+							<li><a href="{{ url('material/acceptance/retur/acceptance/show/' . $row->peneretur_id) }}" class="view-detail"><i class="fa fa-eye"></i>Lihat detail</a></li>
+						</ul>
+					</li>
+				</ul>
+			</td>
+		</tr>
+		@endforeach
 	</tbody>
 </table>
