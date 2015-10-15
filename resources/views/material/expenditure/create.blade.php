b3e27c6f40983ab4efff7a02bf4544d487614348
Aldi Unanto <aldiunanto@yahoo.com>
2015-10-15 10:57:01 +0700

    Tidy up the form

--- resources/views/material/expenditure/create.blade.php
+++ resources/views/material/expenditure/create.blade.php
@@ -60,14 +60,14 @@
 							<button class="btn default add-item"><i class="fa fa-plus"></i> add item</button>
 							<h3>Material</h3>
 						</div>
-						<table class="data-list">
+						<table class="data-list expenditure">
 							<thead>
 								<tr>
 									<th rowspan="2">Material</th>
 									<th rowspan="2">Warna</th>
 									<th rowspan="2">Spesifikasi</th>
 									<th rowspan="2">Satuan</th>
-									<th>Jumlah</th>
+									<th colspan="2">Jumlah</th>
 									<th rowspan="2">Keterangan</th>
 									<th rowspan="2">&nbsp;</th>
 								</tr>
@@ -85,12 +85,12 @@
 
 										<a href="javascript:;" class="btn default open-material"><i class="fa fa-table"></i></a>
 									</td>
-									<td class="sat-jum text-center">&nbsp;</td>
-									<td class="sat-jum text-center">&nbsp;</td>
-									<td class="sat-jum text-center">&nbsp;</td>
-									<td class="sat-jum text-center"><input type="text" name="pengels_permintaan[]" class="text-center" /></td>
-									<td class="sat-jum text-center"><input type="text" name="pengels_realisasi[]" class="text-center" /></td>
-									<td class="sat-jum text-center"><input type="text" name="pengels_ket[]" class="text-center" /></td>
+									<td class="exp-warna text-center">&nbsp;</td>
+									<td class="exp-spek text-center">&nbsp;</td>
+									<td class="exp-satuan text-center">&nbsp;</td>
+									<td class="text-center"><input type="text" name="pengels_permintaan[]" class="text-center" /></td>
+									<td class="text-center"><input type="text" name="pengels_realisasi[]" class="text-center" /></td>
+									<td class="text-center"><input type="text" name="pengels_ket[]" /></td>
 									<td class="text-right"><a href="javascript:;" class="btn danger remove-row"><i class="fa fa-remove"></i></a></td>
 								</tr>
 							</tbody>
