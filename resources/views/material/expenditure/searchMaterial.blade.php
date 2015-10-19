0858885c7d73fd6f4bbb38320253d0d37b8db649
Aldi Unanto <aldiunanto@yahoo.com>
2015-10-19 14:42:36 +0700

    Add some classes to <td>

--- resources/views/material/expenditure/searchMaterial.blade.php
+++ resources/views/material/expenditure/searchMaterial.blade.php
@@ -4,10 +4,10 @@
 		<td><a href="javascript:;" data-id="{{ $row->mat_id }}" class="pick-button btn primary"><i class="fa fa-mail-reply"></i></a></td>
 		<td>{{ $row->mat_nama }}</td>
 		<td>{{ $row->sup_nama }}</td>
-		<td>{{ $row->mat_spesifikasi }}</td>
+		<td class="text-center">{{ $row->mat_spesifikasi }}</td>
 		<td>{{ $row->wrn_nama }}</td>
 		<td>{{ $row->mats_nama }}</td>
-		<td>{{ $row->mat_stock_akhir }}</td>
+		<td class="text-right">{{ $row->mat_stock_akhir }}</td>
 	</tr>
 	@endforeach
 @else
