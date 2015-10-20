071d82953280ea4dde729c6d129dc43f55335d13
Aldi Unanto <aldiunanto@yahoo.com>
2015-10-20 14:28:22 +0700

    Add row item

---
+++ resources/views/material/expenditure/getRowItem.blade.php
@@ -0,0 +1,15 @@
+<tr>
+	<td class="material text-center">
+		<input type="text" name="mat_nama[]" id="mat_nama" readonly="readonly" />
+		<input type="hidden" name="mat_id[]" />
+		<input type="hidden" name="mat_perbandingan[]" />
+
+		<a href="javascript:;" class="btn default open-material"><i class="fa fa-table"></i></a>
+	</td>
+	<td class="exp-warna text-center">&nbsp;</td>
+	<td class="exp-spek text-center">&nbsp;</td>
+	<td class="text-center"><input type="text" name="pengels_permintaan[]" class="text-center" /></td>
+	<td class="text-center"><input type="text" name="pengels_realisasi[]" class="text-center" /></td>
+	<td class="text-center"><input type="text" name="pengels_ket[]" /></td>
+	<td class="text-right"><a href="javascript:;" class="btn danger remove-row"><i class="fa fa-remove"></i></a></td>
+</tr>
