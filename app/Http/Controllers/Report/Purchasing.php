f2a2a760768ab057ac4d13ed9bb70f0268d61e6f
Aldi Unanto <aldiunanto@yahoo.com>
2015-10-27 09:35:50 +0700

    Initalize report-controller for Pruchasing

---
+++ app/Http/Controllers/Report/Purchasing.php
@@ -0,0 +1,12 @@
+<?php namespace App\Http\Controllers\Report;
+
+use App\Http\Controllers\Controller;
+
+class Purchasing extends Controller {
+
+	public function rencanamutu()
+	{
+		return 'Hello World';
+	}
+
+}
