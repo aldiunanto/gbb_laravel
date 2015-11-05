c66baf590ebf5852b6c04fd8cb06437ec5a3e018
Aldi Unanto <aldiunanto@yahoo.com>
2015-11-05 11:30:01 +0700

    Create a lib for months

--- app/Helpers/Utilities.php
+++ app/Helpers/Utilities.php
@@ -64,6 +64,9 @@
     function romawi(){
         return [1 => 'I', 2 => 'II', 3 => 'III', 4 => 'IV', 5 => 'V', 6 => 'VI', 7 => 'VII', 8 => 'VIII', 9 => 'IX', 10 => 'X', 11 => 'XI', 12 => 'XII'];
     }
+    function libMonths(){
+        return [1 => 'Januari', 2 => 'Februari', 3 => 'Maret', 4 => 'April', 5 => 'Mei', 6 => 'Juni', 7 => 'Juli', 8 => 'Agustus', 9 => 'September', 10 => 'Oktober', 11 => 'November', 12 => 'Desember'];
+    }
     function dateDiff($permintaan, $kedatangan){
         $str = '';
         $d = ['y' => ' Tahun', 'm' => ' Bulan', 'd' => ' Hari'];
