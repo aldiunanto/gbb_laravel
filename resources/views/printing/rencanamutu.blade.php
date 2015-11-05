77f8227433ff95a46c150dcfaa155aa102a36289
Aldi Unanto <aldiunanto@yahoo.com>
2015-11-05 11:29:35 +0700

    Modified date format to local zone

--- resources/views/printing/rencanamutu.blade.php
+++ resources/views/printing/rencanamutu.blade.php
@@ -24,7 +24,7 @@
 		<tbody>
 		@foreach($fetch as $row)
 			<tr>
-				<td>{{ $row->po_tgl_buat }}</td>
+				<td>{{ to_indDate($row->po_tgl_buat) }}</td>
 				<td>{{ $row->po_no }}</td>
 
 			<?php $sub = $Posub::fetchDetail($row->po_id); $x = 1; ?>
@@ -38,20 +38,20 @@
 						<td class="left">{{ $item->mat_nama }}</td>
 						<td class="right">{{ $item->pbs_jml }}</td>
 						<td>{{ $item->mats_nama }}</td>
-						<td>{{ $row->po_tgl_kedatangan }}</td>
+						<td>{{ to_indDate($row->po_tgl_kedatangan) }}</td>
 						<td></td>
 					</tr>
 
 				<?php $x++; $partial = $Peners::partialForPrint($item->pos_id); ?>
 				
-				@if($partial->count() > 1)
+				@if($row->po_is_partial == 1)
 					@foreach($partial as $each)
 						<tr>
 							<td colspan="3"></td>
 							<td class="right partial">{{ $each->peners_jml }}</td>
 							<td class="partial"></td>
 							<td class="partial"></td>
-							<td class="partial">{{ $each->pener_date }}</td>
+							<td class="partial">{{ to_indDate($each->pener_date) }}</td>
 						</tr>
 					@endforeach
 				@endif
