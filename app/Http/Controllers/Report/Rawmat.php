b5b2a1cffdb2e644be688f25bfa24d149777dfd1
Aldi Unanto <aldiunanto@yahoo.com>
2015-11-17 14:53:12 +0700

    New controller. Rawmat reporting

---
+++ app/Http/Controllers/Report/Rawmat.php
@@ -0,0 +1,13 @@
+<?php namespace App\Http\Controllers\Report;
+
+use App\Http\Controllers\Controller;
+use App\Libraries\Assets;
+
+class Rawmat extends Controller {
+
+	public function materialTransaction()
+	{
+		return 'Hello World';
+	}
+
+}
