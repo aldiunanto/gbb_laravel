280798f0b566d538e1c586d1bc6056095ae71925
Aldi Unanto <aldiunanto@yahoo.com>
2015-11-18 10:39:12 +0700

    Added to staging area

---
+++ resources/views/printing/materialTransaction.blade.php
@@ -0,0 +1,14 @@
+@extends('printing')
+@section('content')
+
+<header>
+	<div class="caption">
+		<h1>transaksi material</h1>
+		<h2>{{ $period }}</h2>
+	</div>
+</header>
+<section>
+	
+</section>
+
+@endsection
