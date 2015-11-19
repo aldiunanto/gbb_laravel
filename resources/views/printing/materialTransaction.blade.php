09192621763dc7cff2910bad8b1575ad98f3150c
Aldi Unanto <aldiunanto@yahoo.com>
2015-11-19 10:26:53 +0700

    Any..

--- resources/views/printing/materialTransaction.blade.php
+++ resources/views/printing/materialTransaction.blade.php
@@ -23,18 +23,52 @@
 			</tr>
 		</thead>
 		<tbody>
+			<?php $y = 1; ?>
 			@foreach($fetch as $row)
-			<?php $x = 1; ?>
-			<tr>
-				<td class="numb">{{ $x++ }}.</td>
-				<td>{{ $row->mat_nama }}</td>
+			<tr class="item">
+				<td rowspan="2" class="numb">{{ $y++ }}</td>
+				<td class="left">{{ $row->mat_nama }}</td>
 				<td>{{ $row->mats_nama }}</td>
-				<td>{{ $row->mat_spesifikasi }}</td>
+				<td class="left">{{ $row->mat_spesifikasi }}</td>
 				<td>{{ $row->wrn_nama }}</td>
-				<td>{{ $row->sup_nama }}</td>
-				<td>{{ $row->mat_stock_min }}</td>
-				<td>{{ $row->mat_stock_awal }}</td>
-				<td>1425</td>
+				<td class="left">{{ $row->sup_nama }}</td>
+				<td class="right">{{ numbFormat($row->mat_stock_min) }}</td>
+				<td class="right">{{ numbFormat($row->mat_stock_awal) }}</td>
+				<td class="right">{{ numbFormat(1425) }}</td>
+			</tr>
+			<tr>
+				<td colspan="8">
+					<table class="transac-detail">
+						<tr>
+							<td></td>
+							@for($x = 1; $x <= 31; $x++)
+							<td class="date <?php echo (isWeekend($post['year'] . '-' . $post['month'] . '-' . $x) ? 'weekend' : '') ?>">{{ $x }}</td>
+							@endfor
+							<td class="total">Total</td>
+						</tr>
+						<tr>
+							<td>In</td>
+							@for($x = 1; $x <= 31; $x++)
+							<td class="right <?php echo (isWeekend($post['year'] . '-' . $post['month'] . '-' . $x) ? 'weekend' : '') ?>"></td>
+							@endfor
+							<td class="total"></td>
+						</tr>
+						<tr>
+							<td>Out</td>
+							@for($x = 1; $x <= 31; $x++)
+							<td class="right <?php echo (isWeekend($post['year'] . '-' . $post['month'] . '-' . $x) ? 'weekend' : '') ?>"></td>
+							@endfor
+							<td class="total"></td>
+						</tr>
+						<tr>
+							<td>Retur</td>
+							@for($x = 1; $x <= 31; $x++)
+							<td class="right <?php echo (isWeekend($post['year'] . '-' . $post['month'] . '-' . $x) ? 'weekend' : '') ?>"></td>
+							@endfor
+							<td class="total"></td>
+						</tr>
+					</table>
+				</td>
 			</tr>
 			@endforeach
 		</tbody>
