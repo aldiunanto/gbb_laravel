292c61006f5f5d4cf185483515ae98c4cb7288ca
Aldi Unanto <aldiunanto@yahoo.com>
2015-12-17 11:17:46 +0700

    Remove access for [4,5,6,7] to edit material.

--- resources/views/material/index.blade.php
+++ resources/views/material/index.blade.php
@@ -55,10 +55,13 @@
 						<li><span><i class="fa fa-angle-down"></i></span>
 							<ul>
 								<li><a href="{{ url('material/show/' . $row->mat_id) }}" class="view-detail {{ ($role == 4) ? 'no-print' : '' }}"><i class="fa fa-eye"></i>Lihat detail</a></li>
-								@if($role != 4)
+
+								@if($role != 4 && $role != 5 && $role != 6 && $role != 7)
 								<li><a href="{{ url('material/edit/' . $row->mat_id) }}"><i class="fa fa-edit"></i>Ubah data</a></li>
-								<li><a href="{{ url('material/edit/' . $row->sup_id . '/price') }}" title="Ubah semua harga material untuk supplier {{ $row->sup_nama }}"><i class="fa fa-money"></i>Ubah harga</a></li>
 								@endif
+
+								<li><a href="{{ url('material/edit/' . $row->sup_id . '/price') }}" title="Ubah semua harga material untuk supplier {{ $row->sup_nama }}"><i class="fa fa-money"></i>Ubah harga</a></li>
+								
 								@if($role != 3 && $role != 4 && $role != 6 && $role != 7)
 								<li><a href="{{ url('material/destroy/' . $row->mat_id) }}" class="delete"><i class="fa fa-trash"></i>Hapus</a></li>
 								@endif
