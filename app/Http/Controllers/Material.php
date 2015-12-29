79249f697354d1b83ffeded73b419828bbd33ec7
Aldi Unanto <aldiunanto@yahoo.com>
2015-12-29 14:44:45 +0700

    Timestamp true.

--- app/Http/Controllers/Material.php
+++ app/Http/Controllers/Material.php
@@ -742,8 +742,7 @@ class Material extends Controller {
 			'pener_id'			=> $req->input('pener_id'),
 			'returpener_status'	=> 1,
 			'visibility'		=> 1,
-			'is_closed'			=> 2,
-			'created_at'		=> now(true)
+			'is_closed'			=> 2
 		];
 
 		$head = Returpener::create($vals);
