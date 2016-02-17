fd6d4293c935408347b71ac15e7adab7fa1fdd66
Aldi Unanto <aldiunanto@yahoo.com>
2016-02-17 13:21:52 +0700

    Used exclamation mark instead of empty() caused PHP version

--- resources/views/printing/po.blade.php
+++ resources/views/printing/po.blade.php
@@ -173,7 +173,7 @@
 			<td>Vice Director</td>
 		</tr>
 		<tr>
-			<td>{{ session('kar_nama_panggilan') }}</td>
+			<td>{{ (! session('kar_nama_panggilan') ? session('kar_nama') : session('kar_nama_panggilan')) }}</td>
 			<td>Livia Yuwono</td>
 		</tr>
 		<tr>
