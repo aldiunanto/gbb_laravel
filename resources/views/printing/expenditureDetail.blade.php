76d7cd05607c55f7081bc9d2c74c4747886c83d2
Aldi Unanto <aldiunanto@yahoo.com>
2017-03-07 14:59:16 +0700

    Indonesian date format

--- resources/views/printing/expenditureDetail.blade.php
+++ resources/views/printing/expenditureDetail.blade.php
@@ -10,8 +10,9 @@
 				</thead>
 				<tbody>
 					@foreach($fetch as $row)
+						<?php $d = explode('-', $row->pengel_date) ?>
 					<tr>
-						<td class="left">{{ $row->pengel_date }}</td>
+						<td class="left">{{ ($d[2] . '-' . $d[1] . '-' . $d[0]) }}</td>
 						<td>{{ $row->pengels_realisasi }}</td>
 					</tr>
 					@endforeach
